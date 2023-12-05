44efd131b44887e956a910f807b7aa3534b2f7c1
Petr Čírtek <94851162+Pefeci@users.noreply.github.com>
2023-12-05 11:43:51 +0100

    done

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+//start on load
 document.addEventListener('DOMContentLoaded', async function () {
     const videoSources = await fetchCameraInfo();
     createVideoPlayers(videoSources);
 });
-
+// get data from json file
 async function fetchCameraInfo() {
     try {
         const response = await fetch('cams.json');
@@ -17,58 +18,53 @@ async function fetchCameraInfo() {
 function createVideoPlayers(videoSources) {
     const videoPlayersContainer = document.getElementById('videoPlayers');
 
-    // Loop through each video player in the JSON data
+    // loop through each camera in the JSON data to create a video
     Object.keys(videoSources).forEach(cam => {
         const sources = videoSources[cam];
-        console.log(cam)
-        console.log();
-        console.log(sources);
 
         //div to show in grid
         const columnDiv = document.createElement('div');
-        columnDiv.className = 'col-md-6 col-xs-12 divider';
+        columnDiv.className = 'col-md-6 col-sm-12 col-xl-3 divider';
 
-        // Setting video element
+        // setting video element
         const video = document.createElement('video');
         video.id = cam;
-
-        video.className = 'video-js ' + cam + 'video';
+        video.className = 'video-js ' + cam + ' video';
         video.width = 400;
         video.controls = false;
         video.poster = sources.thumbnail;
 
-        // Setting source element
+        // setting source element
         const source = document.createElement('source');
         source.src = sources.stream_url;
+        // add all to the page
         video.appendChild(source);
         columnDiv.appendChild(video)
         videoPlayersContainer.appendChild(columnDiv);
-        // Create player
-        console.log(video.id);
+        // create player
         const player = videojs(video.id, {muted: true});
-
         const playerElement = player.el()
 
-        // Add hover effect to start playing on hover
+        // add hover effect to start playing on hover
         playOnHover(playerElement, player, sources);
-
-        // Append video to the main container
         
     });
 }
 
+// add effects to the videos
 const playOnHover = (videoElement, player, cameraInfo) => {
+    //play on hover
     videoElement.addEventListener('mouseover', () => {
         player.play().catch(error => {
             console.error('Play error:', error.message);
         });
     });
-
+    // stop playing when no hover
     videoElement.addEventListener('mouseout', () => {
         player.pause();
         videoElement.poster = cameraInfo.thumbnail;
     });
-
+    // open modal when clicked
     videoElement.addEventListener('click', () => {
         openModal(player, cameraInfo);
     });
@@ -78,7 +74,7 @@ const playOnHover = (videoElement, player, cameraInfo) => {
 function openModal(player, sources) {
 
 
-    // Get modal elements
+    // get modal elements
     const videoModal = document.getElementById('videoModal');
     const modalVideo = document.getElementById('modalVideo');
     const qualityOptions = document.getElementById('qualityOptions');
@@ -86,36 +82,34 @@ function openModal(player, sources) {
     
     const modalPlayer = videojs('modalVideo')
 
-    // Set video sources for the modal
+    // set video sources for the modal
     modalVideo.innerHTML = '';
 
+    // create sub streams for quality buttons
     const auto = sources.stream_url
     console.log(auto);
     const low = auto.replace(".m3u8", "_low.m3u8");
     const mid = auto.replace(".m3u8", "_mid.m3u8");
     const high = auto.replace(".m3u8", "_high.m3u8");
 
-
+    // lable with url
     const urlLabel = document.getElementById("modalURL")
     urlLabel.innerHTML = 'Video source: ' + auto
-    
 
-    // Create a new Video.js player for the modal
-
-    // Set modal title
+    // set modal title
     modalTitle.textContent = `Location: ${sources.location}, Country: ${sources.country}`;
 
-    // Show modal
+    // show modal
     $(videoModal).modal('show');
 
-    // Initialize Video.js player after the modal is shown
+    // initialize Video.js player after the modal is shown
     $(videoModal).on('shown.bs.modal',async function () {
-        // Initialize Video.js player
         qualityOptions.innerHTML = '';
-        //const modalPlayer = videojs('modalVideo');
         modalPlayer.width = 1000;
-        modalPlayer.src({ src: auto }); // Set the default source
+        modalPlayer.src({ src: auto, muted: false });
         await modalPlayer.play();
+
+        // creation of change quality buttons
         const qualityAutoButton = createQualityButton('Auto', modalPlayer, auto, urlLabel);
         qualityOptions.appendChild(qualityAutoButton);
 
@@ -128,9 +122,8 @@ function openModal(player, sources) {
         const qualityHighButton = createQualityButton('High', modalPlayer, high, urlLabel);
         qualityOptions.appendChild(qualityHighButton);
     });
+    // delete player and create new one for fetching data on hide modal
     $(videoModal).on('hidden.bs.modal',async function  () {
-        //const modalPlayer = videojs('modalVideo');
-        //await modalPlayer.pause();
         modalPlayer.dispose();
         const modalBody = document.getElementById('modalBody');
         const video = document.createElement('video')
@@ -139,10 +132,10 @@ function openModal(player, sources) {
         video.width = "100%";
         video.controls = true;
         modalBody.insertBefore(video, modalBody.firstChild)
-        //modalPlayer.currentTime(0); // Reset the player's current time
     });
 }
 
+// create buttons and adding functions to it
 function createQualityButton(text, player, source, urlLabel) {
     const qualityButton = document.createElement('button');
     qualityButton.className = 'btn btn-outline-primary';
@@ -155,7 +148,6 @@ function createQualityButton(text, player, source, urlLabel) {
 
 
 function changeQuality(player, source, urlLabel) {
-    // Change video source when quality button is clicked
     urlLabel.innerHTML = 'Video source: ' + source
     player.pause();
     player.src({src: source });
